c3cd1c61a1182baf1403769fca4ee6436bcf1e8d
Александр Никифоров <aleksandrnikiforov@Aleksandrs-MacBook-Pro.local>
2023-01-17 15:40:09 +0400

    Adds a photo

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static("./public"));
+
 app.listen(5001, () => {
   console.log("APP is running on 5001");
 });
 
 app.get("/", (req, res) => {
-  res.json("APP");
+  res.sendFile("./public/index.html");
 });
